cdda5a1b305921e59546c426641f484bb7cb5f08
Roy Livne <roylivne@gmail.com>
2021-07-06 10:35:28 +0300

    fixed userStartup

--- startup.py
+++ startup.py
@@ -1,17 +1,5 @@
-import maya.cmds as cmds
-import os
-import system.utils as utils
-reload(utils)
-
 print "Startup"
 
+from ui import ui
 
-# Change the current time unit to ntsc
-cmds.currentUnit( time='ntsc' )
-
-# Change the current linear unit to inches
-cmds.currentUnit( linear='cm' )
-
-import ui.ui as ui
-reload(ui)
-ui.RDojo_UI()
+#reload(ui)
