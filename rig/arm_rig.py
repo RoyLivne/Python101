4ddd51d8f47d1238445fe741998243957bcd379c
Roy Livne <roylivne@gmail.com>
2021-07-07 11:30:49 +0300

    week5 - pov

--- rig/arm_rig.py
+++ rig/arm_rig.py
@@ -1,4 +1,6 @@
 from maya import cmds
+FkControls = ["C_armFK_JNT","C_elbowFK_JNT","C_wristFK_JNT"]
+
 
 def createJointChain(chainType):
     cmds.joint(n='C_arm'+chainType+"_JNT",p=[3, 0, 3])
@@ -15,48 +17,77 @@ def circleControl(name):
     circleCtl = cmds.circle(n=name, ch=0, o=1, nr = [1,0,0])[0]
     return circleCtl
 
+"""Create IK Rig"""
+def createIkHandle(*args):
+    #Create Ik handle
+    armIkHandle = cmds.ikHandle(n='L_arm_IKH', sj='C_armIK_JNT',ee='C_wristIK_JNT',sol="ikRPsolver",p=2,w=True)
+    #get world space potition of wrist joint
+    pos = cmds.xform('C_wristIK_JNT',q=True,t=True,ws=True)
+    #create empty group
+    IKoffGrp = cmds.group(empty=1, n=('C_wristIKOffset_GRP'))
+    #create circle control onject
+    #IKcircleCtl = cmds.circle(n="L_wristIK_CTL",nr=(0,0,1),c=(0,0,0))
+    IKcircleCtl = circleControl("L_wristIK_CTL")
+    cmds.parent(IKcircleCtl ,IKoffGrp)
+    parCon = cmds.parentConstraint('C_wristIK_JNT', IKoffGrp, mo=0)
+    cmds.delete(parCon)
+    #parent Ik to control
+    cmds.parent('L_arm_IKH',"L_wristIK_CTL")
+
+def createFKControls(*args):
+    for fkc in FkControls:
+        circleCtl =  circleControl(fkc);
+        cmds.select(fkc, r=True)
+        cmds.select(cl=1)
+        offGrp = cmds.group(empty=1, n=(fkc + "FKOffset_GRP"))
+        cmds.parent(circleCtl, offGrp)
+        parCon = cmds.parentConstraint(fkc, offGrp, mo=0)
+        cmds.delete(parCon)
+
+def calculatePVPosition(jnts):
+    from maya import cmds,OpenMaya
+    start = cmds.xform(jnts[0],q=True,ws=True,t=True)
+    mid = cmds.xform(jnts[1],q=True,ws=True,t=True)
+    end = cmds.xform(jnts[2], q=True, ws=True, t=True)
+    startv= OpenMaya.MVector(start[0],start[1],start[2])
+    midv = OpenMaya.MVector(mid[0],mid[1],mid[2])
+    endv = OpenMaya.MVector(end[0],end[1],end[2])
+    startEnd  = endv - startv
+    startMid = midv - startv
+    dotP = startMid * startEnd
+    proj = float(dotP)/float(startEnd.length())
+    startEndN = startEnd.normal()
+    projV = startEndN * proj
+    arrowV = startMid - projV
+    arrow *= 0.5
+    finalV = arrowV +midv
+    return([finalV.x,finalV.y,finalV.z])
+
+
 """
 create joint chains
 """
-
 createJointChain("Bind")
 createJointChain("IK")
 createJointChain("FK")
-
 """Create IK Rig"""
-#Create Ik handle
-armIkHandle = cmds.ikHandle(n='L_arm_IKH', sj='C_armIK_JNT',ee='C_wristIK_JNT',sol="ikRPsolver",p=2,w=True)
-#get world space potition of wrist joint
-pos = cmds.xform('C_wristIK_JNT',q=True,t=True,ws=True)
-#create empty group
-IKoffGrp = cmds.group(empty=1, n=('C_wristIKOffset_GRP'))
-#create circle control onject
-#IKcircleCtl = cmds.circle(n="L_wristIK_CTL",nr=(0,0,1),c=(0,0,0))
-IKcircleCtl = circleControl("L_wristIK_CTL")
-cmds.parent(IKcircleCtl ,IKoffGrp)
-parCon = cmds.parentConstraint('C_wristIK_JNT', IKoffGrp, mo=0)
-cmds.delete(parCon)
-#parent Ik to control
-cmds.parent('L_arm_IKH',"L_wristIK_CTL")
-
+createIkHandle("")
 """Create FK Rig"""
-jntName = 'C_arm'
-circleCtl = circleControl(jntName+"FK_CTL")
+createFKControls("")
 
-jntSel = cmds.select( 'C_armFK_JNT',r=True)
+"""
+    jntName = 'C_arm'
+    circleCtl = circleControl(jntName+"FK_CTL")
 
-#jntName = jntSel[0].split("_JNT")
-#ctl = circleCtl
+    jntSel = cmds.select( 'C_armFK_JNT',r=True)
 
-#controlCircle = cmds.rename(ctl, (jntName + "FK_CTL"))
+    cmds.select(cl=1)
 
-cmds.select(cl=1)
+    offGrp = cmds.group(empty=1, n=(jntName + "FKOffset_GRP"))
 
-offGrp = cmds.group(empty=1, n=(jntName + "FKOffset_GRP"))
+    cmds.parent(circleCtl ,offGrp)
 
-cmds.parent(circleCtl ,offGrp)
+    parCon = cmds.parentConstraint('C_armFK_JNT', offGrp, mo=0)
 
-parCon = cmds.parentConstraint('C_armFK_JNT', offGrp, mo=0)
-
-cmds.delete(parCon)
-#Connect IK and FK to Rig joints
+    cmds.delete(parCon)
+"""
